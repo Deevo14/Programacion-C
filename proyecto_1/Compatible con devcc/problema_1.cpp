f94c70074f44a4c2224040fb7d9aa0013ae93d2f
Ricardo Rodríguez <ricardo96r@gmail.com>
2015-02-21 22:16:29 -0430

    Compatible proyecto en dev++

---
+++ proyecto_1/Compatible con devcc/problema_1.cpp
@@ -0,0 +1,198 @@
+/*
+ *  Algoritmos y programación
+ *  PROYECTO I. PARTE I
+ *  Ricardo Rodríguez <ricardo96r@gmail.com>
+ *  Carnet 20141110199
+ *  Cedula 24598546
+ *  Ultima edicion: 21/02/15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float carnet, media, nota[2];
+char resp;
+
+// Datos de cada curso
+struct curso {
+	int alumnos;
+	float media;
+} curso[2];
+
+// Datos del alumno con mayor promedio
+struct alumnoMayor {
+	float carnet;
+	float media;
+} alumMayor;
+
+// Datos del alumno con menor promedio
+struct alumnoMenor {
+	float carnet;
+	float media;
+};
+struct alumnoMenor alumMenor = { 0, 20 };
+
+/*
+ *   Validacion entre dos numeros enteros
+ */
+
+bool validacionEntreNum(float num, float entre1, float entre2, bool *validacion) {
+	if (num >= entre1 && num <= entre2) {
+		return *validacion = true;
+	}
+	else {
+		return *validacion = false;
+	}
+}
+
+/*
+ *   Funcion promedio con validacion de divison entre 0
+ */
+
+float promedio(float num1, float num2) {
+	return num2 == 0 ? 0 : num1 / num2;
+}
+
+void bienvenida() {
+	system("cls");
+	printf("Programa del departamento de programacion: Distribucion de alumnos por curso. \n");
+	printf("%i alumno(s) agregado(s). \n \n", curso[0].alumnos + curso[1].alumnos);
+}
+
+void pedirDatos() {
+	bool validado[3];
+
+	// Primer Scanf: pedir cedula
+	do {
+		printf("- Insertar la cedula del estudiante: ");
+		if (!scanf("%f", &carnet)) {
+			printf("- ERROR: Solo se permiten numeros\n \n");
+			validado[0] = false;
+		}
+		else {
+			validado[0] = true;
+		}
+		fflush(stdin);
+	} while (!validado[0]);
+
+	// Segundo y tercer scanf: pedir notas
+	for (int i = 0; i < 2; i++) {
+		do {
+			printf("\n- Insertar la nota (%i): ", i);
+			if (scanf("%f", &nota[i])) {
+				validado[1] = true;
+				if (!validacionEntreNum(nota[i], 0, 20, &validado[2])) {
+					printf("- ERROR: Solo numeros entre 0-20.\n");
+				}
+			}
+			else {
+				validado[1] = false;
+				printf("- ERROR: Solo se permiten numeros.\n");
+			}
+			fflush(stdin);
+		} while (!validado[1] || !validado[2]);
+	}
+}
+
+/*
+ *	 Funcion para saber cual alumno tiene mayor 
+ *   y menor promedio de notas
+ */
+
+void rankingMedia(char tipo[], float media, float carnet, float *mediaX, float *carnetX){
+	if (tipo == "Mayor") {
+		if (*mediaX <= media) {
+			*mediaX = media;
+			*carnetX = carnet;
+		}
+	}
+	else {
+		if (*mediaX >= media) { // alum_menor = 20
+			*mediaX = media;
+			*carnetX = carnet;
+		}
+	}
+}
+
+void procesarDatos() {
+	media = promedio(nota[0] + nota[1], 2);
+
+	if (media >= 15) { // Curso A
+		curso[0].alumnos++;
+		curso[0].media += media;
+	}
+	else { // Curso B
+		curso[1].alumnos++;
+		curso[1].media += media;
+	}
+
+	rankingMedia("Mayor", media, carnet, &alumMayor.media, &alumMayor.carnet);
+	rankingMedia("Menor", media, carnet, &alumMenor.media, &alumMenor.carnet);
+}
+
+void mostrarDatos() {
+	printf("\n___________________________________________________________\n");
+	printf("                  Reporte por alumno\n");
+	printf("___________________________________________________________\n \n");
+	printf("A1 - Carnet del estudiantes: %.0f\n", carnet);
+	printf("A2 - Notas del curso anterior: %.2f - %.2f\n", nota[0], nota[1]);
+	printf("A3 - Media de notas: %.2f\n", media);
+	printf("A4 - Curso: ");
+	printf(media >= 15 ? "A" : "B");
+	printf("\n___________________________________________________________\n \n");
+}
+
+void deseaContinuar() {
+	bool validado;
+
+	do {
+		printf("\n- Desea agregar a otro alumno. (S)i - (N)o: ");
+		scanf(" %c", &resp);
+		if (!(resp == 's' || resp == 'S' || resp == 'n' || resp == 'N')) {
+			validado = false;
+			printf("- ERROR: El valor insertado no esta permitido. Solo se permiten (S/N)");
+			resp = 's';
+		}
+		else {
+			validado = true;
+		}
+		printf("\n");
+		fflush(stdin);
+	} while (!validado);
+}
+
+void mostrarReporte() {
+	printf("_________________________________________\n");
+	printf("             REPORTE FINAL \n");
+	printf("_________________________________________\n \n");
+
+	printf("B1 - Cantidad de alumnos: \n");
+	printf("B1 - Curso A: %i \n", curso[0].alumnos);
+	printf("B1 - Curso B: %i \n \n", curso[1].alumnos);
+
+	printf("B2 - Promedio de notas: \n");
+	printf("B2 - Curso A: %.2f \n", promedio(curso[0].media, (float)curso[0].alumnos));
+	printf("B2 - Curso B: %.2f \n \n", promedio(curso[1].media, (float)curso[1].alumnos));
+
+	printf("B3 - Alumno con mayor promedio: \n");
+	printf("B3 - Carnet: %.0f \n", alumMayor.carnet);
+	printf("B3 - Promedio: %.2f \n \n", alumMayor.media);
+
+	printf("B4 - Alumno con menor promedio: \n");
+	printf("B4 - Carnet: %.0f \n", alumMenor.carnet);
+	printf("B4 - Promedio: %.2f \n", alumMenor.media);
+	printf("_________________________________________\n \n");
+}
+
+main() {
+	do {
+		bienvenida();
+		pedirDatos();
+		procesarDatos();
+		mostrarDatos();
+		deseaContinuar();
+	} while (resp == 's' || resp == 'S');
+	bienvenida();
+	mostrarReporte();
+	system("pause");
+}
---
+++ proyecto_1/Compatible con devcc/problema_2.cpp
@@ -0,0 +1,226 @@
+/*
+ *  Algoritmos y programación
+ *  PROYECTO I. PARTE II
+ *  Ricardo Rodríguez <ricardo96r@gmail.com>
+ *  Carnet 20141110199
+ *  Cedula 24598546
+ *  Ultima edicion: 21/02/15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float alm, costoP, ganancia, costoPNinos, gananciaDulces;
+char tipo, resp;
+int rango, ninos, adultos, salados, dulces;
+
+// Datos del tipo (A - N) que tiene mas cantidad
+struct tipoMayorCantidad {
+	int cantidad;
+	char tipo;
+} tipoMayor;
+
+// Datos del producto de mayor ganacia
+struct productoMayorGanancia {
+	int rango;
+	float ganancia;
+	char tipo;
+} gananciaMayor;
+
+/*
+ *	Validacion entre dos numeros enteros
+ */
+
+bool validacionEntreNum(int num, int entre1, int entre2, bool *validacion) {
+	if (num >= entre1 && num <= entre2) {
+		return *validacion = true;
+	}
+	else {
+		return *validacion = false;
+	}
+}
+
+/*
+ *   Funcion promedio con validacion de divison entre 0
+ */
+
+float promedio(float num1, float num2) {
+	return num2 == 0 ? 0 : num1 / num2;
+}
+
+void bienvenida() {
+	system("cls");
+	printf("Programa de una empresa del ramo de alimentos sobre los costos por producto. \n");
+    printf("%i producto(s) agregado(s)\n\n", dulces + salados);
+}
+
+void pedirDatos() {
+	bool validado[4];
+
+	// Primer scanf: Almacenamiento
+	do {
+		printf("- Insertar el costo de almacenamiento del producto: ");
+		if (!scanf("%f", &alm)) {
+			validado[0] = false;
+			printf("- ERROR: Solo se permiten numeros reales\n \n");
+		}
+		else {
+			validado[0] = true;
+		}
+		fflush(stdin);
+	} while (!validado[0]);
+
+	// Segundo scanf: tipo de producto
+	do {
+		printf("\n- Insertar el tipo de producto: (N)inos - (A)dultos: ");
+		scanf("%c", &tipo);
+		if (!(tipo == 'n' || tipo == 'N' || tipo == 'a' || tipo == 'A')) {
+			validado[1] = false;
+			printf("- ERROR: El valor insertado no esta permitido. Solo se permiten (S/N)\n");
+		}
+		else {
+			validado[1] = true;
+		}
+		fflush(stdin);
+	} while (!validado[1]);
+
+	// Tercer scanf: Rango
+	do {
+		printf("\n- Insertar el rango del producto (1-20): ");
+		if (scanf("%i", &rango)) {
+			validado[2] = true;
+			if (!validacionEntreNum(rango, 1, 20, &validado[3])) {
+				printf("- ERROR: Solo numeros entre 1-20.\n");
+			}
+		}
+		else {
+			validado[2] = false;
+			printf("- ERROR: Solo se permiten numeros.\n");
+		}
+		fflush(stdin);
+	} while (!validado[2] || !validado[3]);
+}
+
+void procesarDatos() {
+	// Salados
+	if (rango <= 10) {
+		salados++;
+		if (tipo == 'n' || tipo == 'N') { // Salado Niños
+			costoP = 2 * alm;
+			ganancia = costoP*0.10;
+			ninos++;
+			costoPNinos += costoP;
+		}
+		else { // Salado Adultos
+			costoP = 4 * alm;
+			ganancia = costoP*0.20;
+			adultos++;
+		}
+	}
+	// Dulces
+	else {
+		dulces++;
+		if (tipo == 'n' || tipo == 'N') { // Dulce Niños
+			costoP = 4 * alm;
+			ganancia = costoP*0.15;
+			ninos++;
+			costoPNinos += costoP;
+		}
+		else { // Dulce adulto
+			costoP = 6 * alm;
+			ganancia = costoP*0.25;
+			adultos++;
+		}
+		gananciaDulces += ganancia;
+	}
+
+	// Ganacia mayor
+	if (gananciaMayor.ganancia <= ganancia) {
+		gananciaMayor.ganancia = ganancia;
+		gananciaMayor.tipo = tipo;
+		gananciaMayor.rango = rango;
+	}
+}
+
+void mostrarDatos() {
+	printf("\n_________________________________________\n");
+	printf("          Reporte por producto\n");
+	printf("_________________________________________\n \n");
+	printf("A1 - Tipo de producto: ");
+	printf(tipo == 'a' || tipo == 'A' ? "(A)dulto" : "(N)inos");
+	printf("\nA2 - Codigo de variedad : %i - ", rango);
+	printf(rango >= 11 ? "dulce" : "salado");
+	printf("\nA3 - Costo de almacenamiento %.2f\n", alm);
+	printf("A4 - Valor de produccion: %.2f\n", costoP);
+	printf("A5 - Precio de venta al publico: %.2f\n", costoP + ganancia);
+	printf("A6 - Ganacia de la empresa: %.2f\n", ganancia);
+	printf("\n_________________________________________\n \n");
+}
+
+void deseaContinuar() {
+	bool validado;
+
+	do {
+		printf("\n- Desea agregar a otro alumno. (S)i - (N)o: ");
+		scanf(" %c", &resp);
+		if (!(resp == 's' || resp == 'S' || resp == 'n' || resp == 'N')) {
+			validado = false;
+			printf("- ERROR: El valor insertado no esta permitido. Solo se permiten (S/N)");
+			resp = 's';
+		}
+		else {
+			validado = true;
+		}
+		printf("\n");
+		fflush(stdin);
+	} while (!validado);
+}
+
+void tipoMayorCantidad(int ninos, int adultos, char *tipo, int *cantidad) {
+	if (ninos >= adultos) {
+		*tipo = 'n';
+		*cantidad = ninos;
+	}
+	else {
+		*tipo = 'a';
+		*cantidad = adultos;
+	}
+}
+
+void mostrarReporte() {
+	printf("______________________________________________________________\n");
+	printf("                        REPORTE FINAL \n");
+	printf("______________________________________________________________\n \n");
+	printf("B1 - Producto que se fabrico en mayor cantidad: \n");
+	printf("B1 - Tipo: ");
+	printf(tipoMayor.tipo == 'a' || tipoMayor.tipo == 'A' ? "(A)dulto" : "(N)inos");
+	printf("\nB1 - Cantidad: %i \n \n", tipoMayor.cantidad);
+	printf("B2 - Cantidad de variedades \n");
+	printf("B2 - Salado: %i \n", salados);
+	printf("B2 - Dulce: %i \n \n", dulces);
+	printf("B3 - Total del costo de produccion para ninos:  %.2f\n", costoPNinos);
+	printf("B4 - Total de ganancia de los productos dulces: %.2f\n", gananciaDulces);
+	printf("B5 - Promedio del costo de produccion para ninos: %.2f \n", promedio(costoPNinos, (float)ninos));
+	printf("B6 - Promedio de ganancias por dulces: %.2f \n\n", promedio(gananciaDulces, (float)dulces));
+	printf("B7 - Producto que genero la maxima ganancia: \n");
+	printf("B7 - Tipo: ");
+	printf(gananciaMayor.tipo == 'a' || gananciaMayor.tipo == 'A' ? "(A)dulto" : "(N)inos");
+	printf("\nB7 - Codigo: (%i) - ", gananciaMayor.rango);
+	printf(gananciaMayor.rango <= 10 ? "Salado" : "Dulce");
+	printf("\nB7 - Ganancia: %.2f \n \n", gananciaMayor.ganancia);
+	printf("______________________________________________________________\n \n");
+}
+
+main() {
+	do {
+		bienvenida();
+		pedirDatos();
+		procesarDatos();
+		mostrarDatos();
+		deseaContinuar();
+	} while (resp == 's' || resp == 'S');
+	tipoMayorCantidad(ninos, adultos, &tipoMayor.tipo, &tipoMayor.cantidad);
+	bienvenida();
+	mostrarReporte();
+	system("pause");
+}
