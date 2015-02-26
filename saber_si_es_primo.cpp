278376bb15853608ba2a43fb53c3181941121caf
Ricardo Rodríguez <ricardo96r@gmail.com>
2015-02-26 11:21:39 -0430

    Arreglado primos

--- saber_si_es_primo.cpp
+++ saber_si_es_primo.cpp
@@ -8,12 +8,17 @@ Visual C++: Para que deje complilar
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
 
-int num;
+double num;
+char resp;
 
-bool es_par(int num) {
-	if (num % 2 == 0) {
+/*
+*	Saber si un numero es par devolviendo
+*  un tipo bool
+*/
+
+bool es_par(double num) {
+	if (fmod(num, 2) == 0) { // Fmod saca el resto de un numero Double o float. El "%" no permite float ni double
 		return true;
 	}
 	else {
@@ -21,40 +26,39 @@ bool es_par(int num) {
 	}
 }
 
-int es_primo(int num) {
-	bool primo = false;
-	int n = 3;
+/*
+*	Funcion para saber si es un numero primo.
+*	REGLA DE PRIMOS:
+*  - Tiene que ser impar por la funcion es_par()
+*	- Si es impar se dividen todos los numeros del 3 hasta la raiz cuadrada de ese numero
+*    y si su resto no es igual a 0 es primo. sqrt(num) = raiz del numero.
+*/
+
+bool es_primo(double num) {
+	bool primo = false; // Incializar
+	double n = 3; // Comienza con dividir con 3 hasta la raiz cuadrada
 	if (!es_par(num)) {
-		primo = true;
+		primo = true; // Es posible que sea primo por ser impar
 		do {
-			if (num % n == 0) {
-				primo = false;
-				//printf("Falso \n");
-			}
 			//printf("Division: %i\n", n);
+			if (fmod(num, n) == 0) {
+				primo = false; // No es primo
+				//printf("Parar bucle\n");
+			}
 			n++;
-		} while (n <= sqrt((float)num) && primo); // sqrt = raiz de math.h;
+		} while (n <= sqrt(num) && primo); // Hasta su raiz cuadrada o hasta que primo = false
 	}
-	if (primo) {
+	if (primo || num == 2 || num == 3) {
 		return true;
 	}
 	else {
 		return false;
-	}
-}
-
-bool validacionInt(int *scan, bool *validacion) {
-	if (!scanf("%i", scan)) {
-		return *validacion = false;
-	}
-	else {
-		return *validacion = true;
 	}
 }
 
 void bienvenida() {
 	system("cls");
-	printf("Programa que te muestra si es un numero primo o no.\n \n");
+	printf("Programa que te muestra si es un numero es primo o no.\n \n");
 }
 
 void pedirDatos() {
@@ -62,32 +66,34 @@ void pedirDatos() {
 
 	do {
 		printf("- Insertar un numero: ");
-		if (!validacionInt(&num, &validado)) {
+		validado = true; // Reiniciar variable
+		if (!scanf("%lf", &num)) {
 			printf("- ERROR: Solo se permiten numeros\n \n");
+			validado = false;
 		}
+		else {
+			if (!(num > 0)){
+				validado = false;
+				printf("- ERROR: Solo numeros enteros positivos\n \n");
+			}
+		}
+		fflush(stdin);
 	} while (!validado);
 }
 
 void mostrarDatos() {
-	time_t start_t, end_t;
-	
-	time(&start_t);
 	if (es_primo(num)) {
-		printf("- El numero %i es primo\n", num);
+		printf("- El numero %.0f es primo\n", num);
 	}
 	else {
-		printf("- El numero %i no es primo\n", num);
+		printf("- El numero %.0f no es primo\n", num);
 	}
-	time(&end_t);
-	printf("\nEl programa ha tardado: %lf segundos.\n", difftime(end_t, start_t));
 }
 
-bool deseaContinuar() {
+void deseaContinuar() {
 	bool validado;
-	char resp;
-
 	do {
-		printf("\n- Desea agregar otro numero? (S)i - (N)o: ");
+		printf("\n- Desea intentar con otro numero? (S)i - (N)o: ");
 		scanf(" %c", &resp);
 		if (!(resp == 's' || resp == 'S' || resp == 'n' || resp == 'N')) {
 			validado = false;
@@ -100,13 +106,6 @@ bool deseaContinuar() {
 		printf("\n");
 		fflush(stdin);
 	} while (!validado);
-
-	if (resp == 'n' || resp == 'N') {
-		return true;
-	}
-	else {
-		return false;
-	}
 }
 
 void main() {
@@ -114,5 +113,6 @@ void main() {
 		bienvenida();
 		pedirDatos();
 		mostrarDatos();
-	} while (!deseaContinuar());
+		deseaContinuar();
+	} while (resp == 's' || resp == 'S');
 }
